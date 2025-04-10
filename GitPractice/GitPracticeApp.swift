0670518a460292ed85ef0efeccd1ce1659457af7
Valerie Hammer <vkellogg@wisc.edu>
2025-04-10 14:44:39 -0500

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Valerie Hammer on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
